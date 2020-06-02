9e6feed871a0133dd1188d5877a61e13dd527179
vscalado <vitor.santos.calado@gmail.com>
2020-06-01 22:26:27 -0300

    Correção Selenium Grid

--- src/test/java/br/ce/wcaquino/tasks/prod/HealthCheckIT.java
+++ src/test/java/br/ce/wcaquino/tasks/prod/HealthCheckIT.java
@@ -10,13 +10,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HealthCheckIT {
 
 	@Test
 	public void healthCheck() throws MalformedURLException {
-		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.15.11:4444/wd/hub"), cap);
+		WebDriver driver = new  ChromeDriver();
+		//DesiredCapabilities cap = DesiredCapabilities.chrome();
+		//WebDriver driver = new RemoteWebDriver(new URL("http://192.168.15.11:4444/wd/hub"), cap);
 		try {
 			driver.navigate().to("http://192.168.15.11:9999/tasks/");
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
